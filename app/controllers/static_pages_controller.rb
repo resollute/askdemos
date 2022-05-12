7ffc3677a18c3f8ef8af7be1e1edb503d21bc4ba
resollute <vteleleiko@gmail.com>
2022-05-12 15:24:05 +0300

    static pages controller

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,15 @@
+class StaticPagesController < ApplicationController
+  # rails g controller static_pages landing_page pricing privacy terms
+
+  def landing_page
+  end
+
+  def pricing
+  end
+
+  def privacy
+  end
+
+  def terms
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'static_pages#landing_page'
+  get 'pricing', to: 'static_pages#pricing'
+  get 'privacy', to: 'static_pages#privacy'
+  get 'terms', to: 'static_pages#terms'
 end

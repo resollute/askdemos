203b9aa24cf8a758a4ded46c5962f7714f330a07
resollute <vteleleiko@gmail.com>
2022-05-12 16:59:11 +0300

    inbox validations

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_12_133254) do
   enable_extension "plpgsql"
 
   create_table "inboxes", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

7f2e308210f0f757d688f9cadb4066c3a808a048
resollute <vteleleiko@gmail.com>
2022-05-13 14:32:43 +0300

    update ruby version to the latest stable

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
   # rails g controller static_pages landing_page pricing privacy terms
+  skip_before_action :authenticate_user!
 
   def landing_page; end
 
---
+++ devise/lib/devise.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "devise/version"
+
+module Devise
+  class Error < StandardError; end
+  # Your code goes here...
+end

dd5f638c3bc742cc17579f49b47ad6fdc19820f4
resollute <vteleleiko@gmail.com>
2022-05-13 11:22:43 +0300

    validate inbox name uniqueness on db level

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_12_133254) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_inboxes_on_name", unique: true
   end
 
 end

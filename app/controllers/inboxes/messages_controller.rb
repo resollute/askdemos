909bb002ebf2b43fa1acbc54be923b701e46c970
resollute <vteleleiko@gmail.com>
2022-05-15 13:59:30 +0300

    clean up unused code

--- app/controllers/inboxes/messages_controller.rb
+++ app/controllers/inboxes/messages_controller.rb
@@ -1,43 +1,43 @@
-class Inboxes::MessagesController < ApplicationController
-  # rails g scaffold message body:text inbox:references user:references
-  before_action :set_inbox
+module Inboxes
+  class MessagesController < ApplicationController
+    # rails g scaffold message body:text inbox:references user:references
+    before_action :set_inbox
 
-  # GET /messages/new
-  def new
-    @message = @inbox.messages.new
-  end
+    def new
+      @message = @inbox.messages.new
+    end
 
-  def edit
-  end
+    def edit; end
 
-  def create
-    @message = @inbox.messages.new(message_params)
+    def create
+      @message = @inbox.messages.new(message_params)
 
-    respond_to do |format|
-      if @message.save
-        format.html { redirect_to @inbox, notice: "Message was successfully created." }
-      else
-        format.html { render :new, status: :unprocessable_entity }
+      respond_to do |format|
+        if @message.save
+          format.html { redirect_to @inbox, notice: 'Message was successfully created.' }
+        else
+          format.html { render :new, status: :unprocessable_entity }
+        end
       end
     end
-  end
 
-  def destroy
-    @message = Message.find(params[:id])
-    @message.destroy
+    def destroy
+      @message = Message.find(params[:id])
+      @message.destroy
 
-    respond_to do |format|
-      format.html { redirect_to @inbox, notice: "Message was successfully destroyed." }
+      respond_to do |format|
+        format.html { redirect_to @inbox, notice: 'Message was successfully destroyed.' }
+      end
     end
-  end
 
-  private
+    private
 
-  def set_inbox
-    @inbox = Inbox.find(params[:inbox_id])
-  end
+    def set_inbox
+      @inbox = Inbox.find(params[:inbox_id])
+    end
 
-  def message_params
-    params.require(:message).permit(:body).merge(user: current_user)
+    def message_params
+      params.require(:message).permit(:body).merge(user: current_user)
+    end
   end
 end

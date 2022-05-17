fb38772250f8a32034a927e6e43fa01116cf9cd9
resollute <vteleleiko@gmail.com>
2022-05-17 16:34:54 +0300

    default order of messages via inbox model

--- app/controllers/inboxes/messages_controller.rb
+++ app/controllers/inboxes/messages_controller.rb
@@ -52,12 +52,7 @@ module Inboxes
       @message.destroy
 
       respond_to do |format|
-        format.turbo_stream do
-          render turbo_stream: [
-            turbo_stream.remove(@message),
-            turbo_stream.update('message_counter', @inbox.messages_count)
-          ]
-        end
+        format.turbo_stream
         format.html { redirect_to @inbox, notice: 'Message was successfully destroyed.' }
       end
     end

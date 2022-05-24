10917e6a0ff1918f50c9ebce13ad6ddf3d451c32
resollute <vteleleiko@gmail.com>
2022-05-24 18:10:01 +0300

    fix turbo for inboxes count

--- app/controllers/inboxes_controller.rb
+++ app/controllers/inboxes_controller.rb
@@ -49,7 +49,8 @@ class InboxesController < ApplicationController
 
   def destroy
     @inbox.destroy
-
+    @inboxes = Inbox.all
+    @inboxes_count = @inboxes.count
     respond_to do |format|
       flash.now[:notice] = "Inbox #{@inbox.id} destroyed!"
       format.turbo_stream

7752f245c77f36e1d3ba50a54cf67d0c927e7350
resollute <vteleleiko@gmail.com>
2022-05-28 13:16:27 +0300

    edit credentials 2

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -273,12 +273,12 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :github,
-    Rails.application.credentials.dig(:omniauth, :github, :client),
+    Rails.application.credentials.dig(:omniauth, :github, :client_id),
     Rails.application.credentials.dig(:omniauth, :github, :secret),
     scope: 'repo,user'
 
   config.omniauth :google_oauth2,
-    Rails.application.credentials.dig(:omniauth, :google, :client),
+    Rails.application.credentials.dig(:omniauth, :google, :client_id),
     Rails.application.credentials.dig(:omniauth, :google, :secret)
     # scope: 'userinfo.email,userinfo.profile'
 
